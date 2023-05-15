758fcafe86b869d1da07e4d2f08bc1d328a14eff
Agostina Boris <agostinaboris@protonmail.com>
2023-05-14 23:13:00 -0300

    se modifica componente Instagram.jsx

--- components/Instagram.jsx
+++ components/Instagram.jsx
@@ -11,8 +11,8 @@ import InstagramImg from './InstagramImg';
 const Instagram = () =>{
     return (
         <div className="max-w-[1240px] mx-auto text-center py-24">
-        <p className="text-3xl font-bold">Follow me on Instagram</p>
-        <p className="pb-4">@Nikko</p>
+        <p className="text-white text-3xl font-bold">Follow me on Instagram</p>
+        <p className="pb-4 text-white">@Nikko</p>
         <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-6 gap-2 p-4">
             <InstagramImg socialImg={IGImg1}/>
             <InstagramImg socialImg={IGImg2}/>
